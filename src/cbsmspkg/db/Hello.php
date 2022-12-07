4b0ed80b5d7684f8ebd753132f5e47707149d645
azhai2016 <1229991003@qq.com>
2022-12-07 09:15:30 +0800

    init

---
+++ src/cbsmspkg/db/Hello.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace cbsmspkg\db;
+
+class Hello{
+    public function __construct(){
+
+    }
+
+    public static function say() {
+        
+        echo 'Hi World!';
+    }
+}
+
+
+?>
